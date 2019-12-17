fef21cdfe2834d9c3cbcd2adcdaa47b368fd0282
yu.zhang <zhangyucumt@foxmail.com>
2019-12-17 23:05:41 +0800

    Update user.py

--- {{cookiecutter.project}}/{{cookiecutter.project}}/consts/user.py
+++ {{cookiecutter.project}}/{{cookiecutter.project}}/consts/user.py
@@ -1,12 +1,15 @@
-from {{cookiecutter.project}}.consts import EnumObject
+from athena.consts import EnumObject
 
 
 class Sex(EnumObject):
     female = 0
-    make = 1
+    male = 1
     unknown = 2
+    __transaction__ = dict(female='女', male='男', unknown='未知')
 
 
 class OpenidType(EnumObject):
     phone = 1
     email = 2
+    __transaction__ = dict(phone='手机', email='邮箱')
+
