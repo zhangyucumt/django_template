a0a9b55fc2b76d252d811707f28d989f9c9479c2
yu.zhang <zhangyucumt@foxmail.com>
2020-04-03 11:07:45 +0800

    Update decorators.py

--- {{cookiecutter.project}}/{{cookiecutter.project}}/decorators.py
+++ {{cookiecutter.project}}/{{cookiecutter.project}}/decorators.py
@@ -44,3 +44,15 @@ def allows_filters(func):
     def wrapper(*args, **kwargs):
         return func(*args, **kwargs)
     return wrapper
+
+
+class memorize(dict):
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args):
+        return self[args]
+
+    def __missing__(self, key):
+        result = self[key] = self.func(*key)
+        return result
